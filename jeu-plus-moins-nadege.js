255cef5f31b9546590b63c52d80d67a5d68255a4
simplon-mars-2-nadege-s <princessgirlyfactory@gmail.com>
2016-11-20 16:18:21 +0100

    Update jeu-plus-moins-nadege.js

--- jeu-plus-moins-nadege.js
+++ jeu-plus-moins-nadege.js
@@ -40,3 +40,74 @@ for (var i = 1; i < 7; i++) {
 if((saisie !== solution) && (i >=7) )
 prompt("Perdu ! Les 6 coups sont utilisés,la solution était " + solution)
 console.log("Perdu ! Les 6 coups sont utilisés,la solution était " + solution);
+
+
+
+
+Autres solutions
+
+/*
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+console.log("(La solution est " + solution + ")");
+
+var choix = Number(prompt("Entrez un nombre entre 1 et 100 :"));
+for (var i=1;i<=6 && choix !== solution;i++) {
+	if (choix<solution){
+		console.log(choix + " est trop petit.");
+	} else {
+		console.log(choix + " est trop grand.");
+	}
+	choix = Number(prompt("Réesseyez :"));
+}
+if (choix===solution) {
+	console.log("Bravo ! La solution était " + solution +".\nVous avez trouvé en " + i + " essai(s).")
+} else {
+	console.log("Perdu, la solution était " + solution +".")
+}
+
+
+
+
+Autres solutions 
+
+console.log("Bienvenue dans ce jeu de devinette !");
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+console.log("(La solution est " + solution + ")");
+// TODO : complétez le programme
+var num = 0;
+var cont = true;
+var i = 1;
+do {
+    if (i <= 6) {
+        num = Number(prompt("Entrez un nombre:"));
+        if (num > solution) {
+            console.log(num + " est trop grand!");
+        }
+        else if (num < solution) {
+            console.log(num + " est trop petit!");
+        }
+        else {
+            console.log("Bravo, la solution était: " + solution + ".\nVous avez réussi en " + i + " essais.");
+            cont = false;
+        }
+        i++;
+    }
+    else {
+        console.log("Perdu, la solution était " + solution + ".");
+        cont = false;
+    }
+} while (cont);
+
